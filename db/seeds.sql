33e92946cb624af2ae52f474a9932b8a949e0e16
Shawanda Ford <shawanda.ford@gmail.com>
2020-10-13 00:17:05 -0400

    setup folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
